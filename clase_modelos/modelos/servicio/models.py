8fb370a6e687d493beb00525b1b66088c0abd64e
Juan Ignacio Bonini <juanignaciobonini@hotmail.com>
2024-05-03 20:46:12 -0300

    Clase modelos 2

--- clase_modelos/modelos/servicio/models.py
+++ clase_modelos/modelos/servicio/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Servicio(models.Model):
+    nombre = models.CharField(max_length=50)
+    descripcion = models.TextField()
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    duracion = models.DurationField()
+
+    def __str__(self) -> str:
+        return self.nombre
--- clase_modelos/modelos/turno/models.py
+++ clase_modelos/modelos/turno/models.py
@@ -8,6 +8,9 @@ class Turno(models.Model):
     servicio = models.ForeignKey("servicio.Servicio", on_delete=models.CASCADE)
     cliente = models.ForeignKey("cliente.Client", on_delete=models.CASCADE)
 
+    # models.OneToOneField("calendario.Calendario", on_delete=models.CASCADE)
+    # models.ManyToManyField("peluquero.Peluquero")
+
     @staticmethod
     def bloque_valido(fecha, hora, servicio_id):
         """Pre condiciones:
@@ -30,6 +33,26 @@ class Turno(models.Model):
                 11:30 - 12:00
                 ETC
         """
+
+
+"""
+En la documentación de Django, puedes encontrar detalles sobre los tipos de relaciones que puedes establecer entre modelos. Aquí te resumo las principales:
+
+Relaciones uno a uno (One-to-one): Utiliza OneToOneField para definir una relación uno a uno.
+Por ejemplo, si tienes un modelo Place y un modelo Restaurant,
+cada Place puede estar asociado a un solo Restaurant y viceversa​ https://docs.djangoproject.com/en/5.0/topics/db/examples/one_to_one/
+
+Relaciones uno a muchos (Many-to-one): Usa ForeignKey para este tipo de relación.
+Un ejemplo sería un modelo Reporter que puede tener asociados muchos Article,
+pero cada Article solo está asociado a un Reporter​ https://docs.djangoproject.com/en/5.0/topics/db/examples/many_to_one/​.
+
+Relaciones muchos a muchos (Many-to-many): Se define con ManyToManyField.
+Por ejemplo, un modelo Article puede estar asociado con muchos Publication y
+cada Publication puede tener muchos Article.
+Además, puedes agregar y quitar objetos de estas relaciones
+sin necesidad de guardar cada instancia,
+facilitando la manipulación de las relaciones​ https://docs.djangoproject.com/en/5.0/topics/db/examples/many_to_many/​.
+"""
 
 
 """
